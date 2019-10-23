70028cb6b5d93c39030e20412aa5d5b5d2ff360d
Francesco <franc.ns@gmail.com>
2019-10-23 15:27:05 +0200

    changed output in page as ul with li

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 // Scrivi anche la posizione della lista in cui il nuovo utente si trova
 
 // variabili
-var userSurname, listSurname, check, i;
+var userSurname, listSurname, check, i, j;
 
 // creazione array cognomi
 listSurname = ['Rossi', 'Ferrari', 'Russo', 'Bianchi', 'Costa', 'Fontana', 'Rota', 'Lombardi', 'Mancini', 'Brambilla', 'Fumagalli'];
@@ -14,22 +14,27 @@ userSurname = prompt('Inserisci il tuo cognome');
 listSurname.push(userSurname);
 
 // output lista non ordinata
-document.getElementById('js-listSurname').innerHTML = (listSurname);
+// document.getElementById('js-listSurname').innerHTML = (listSurname);
+for (i = 0; i < listSurname.length; i++) {
+  document.getElementById('js-listSurname').innerHTML += ('<li>' + listSurname[i] + '</li>');
+}
 
 // ordinamento lista cognomi
 listSurname.sort();
 
 // output lista in ordine alfabetico
-document.getElementById('js-listSurnameOrder').innerHTML = (listSurname);
+// document.getElementById('js-listSurnameOrder').innerHTML = (listSurname);
+for (i = 0; i < listSurname.length; i++) {
+  document.getElementById('js-listSurnameOrder').innerHTML += ('<li>' + listSurname[i] + '</li>');
+}
 
 // confronto del cognome utente con lista ordinata
-i = 0;
+j = 0;
 check = false;
-while (i < listSurname.length && check ===false) {
-  if (listSurname[i] === userSurname){
+while (j < listSurname.length && check ===false) {
+  if (listSurname[j] === userSurname){
     check = true;
-    console.log(i);
-    document.getElementById('js-position').innerHTML = (i+1 + ' della lista');
+    document.getElementById('js-position').innerHTML = (j+1 + ' della lista');
   }
-  i++;
+  j++;
 }
